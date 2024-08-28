6d00d1d665b835672e1c0be0eff9b305e76ac16b
Victor Flores <victorflores20511@gmail.com>
2024-08-27 23:27:57 -0500

    Initial Commit

---
+++ Proyecto 1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Proyecto 1
+//
+//  Created by Victor Flores on 08/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Proyecto 1/Proyecto_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Proyecto_1App.swift
+//  Proyecto 1
+//
+//  Created by Victor Flores on 08/27/24.
+//
+
+import SwiftUI
+
+@main
+struct Proyecto_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
